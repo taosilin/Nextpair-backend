1bfcef0fa79235c23a16a55ec749bd5c548bca26
taosilin <852321819@qq.com>
2021-02-01 18:42:45 +0800

    2.1

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -18,31 +18,31 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-//    // SpringBoot2.x配置HTTPS,并实现HTTP访问自动转向HTTPS
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(httpConnector());
-//        return tomcat;
-//    }
-//
-//    @Bean
-//    public Connector httpConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8088); // 监听Http的端口
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443); // 监听Http端口后转向Https端口
-//        return connector;
-//    }
+    // SpringBoot2.x配置HTTPS,并实现HTTP访问自动转向HTTPS
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8088); // 监听Http的端口
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // 监听Http端口后转向Https端口
+        return connector;
+    }
 }
